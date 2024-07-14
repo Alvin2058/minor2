7dd1ec57b84152b0548789a48c1a8d1182a29f1c
DeepakKhanl <khanald052@gmail.com>
2024-07-14 11:55:19 +0545

    Change in quiz

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.Quiz.as_view(), name="quiz"),
-   # path("add_question/", views.AddQuestion.as_view(), name="add_question"),
+    #path("add_question/", views.AddQuestion.as_view(), name="add_question"),
     path("result/", views.Result.as_view(), name="result"),
-    path("leaderboard/", views.Leaderboard.as_view(), name="leaderboard")
+    path("leaderboard/", views.Leaderboard.as_view(), name="leaderboard")  
 ]
